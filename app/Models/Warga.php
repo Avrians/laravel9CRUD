a1c915b550dccbda0fe7eb6209796d57810ce135
avrians <avriansyahb10@gmail.com>
2023-04-30 08:53:29 +0700

    update tutor 5 model

---
+++ app/Models/Warga.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Warga extends Model
+{
+    use HasFactory;
+    // defaulnya laravel itu memakai bahasa inggris dan yang di model itu singular dan yang ada di table database itu jamak
+    // sehingga yang di table itu ada tambahan 's'
+    // dan jika menggunakan bahasa indonesia nama table dan model itu disamakan dan perlu ada tambahan kodingan dibawah ini
+    protected $table = 'warga';
+}
