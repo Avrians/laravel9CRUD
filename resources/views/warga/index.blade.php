100887a6518d85961124e502fbf9ee56f64d2493
avrians <avriansyahb10@gmail.com>
2023-04-30 19:55:22 +0700

    finish tutot 9 delete data di db

--- resources/views/warga/index.blade.php
+++ resources/views/warga/index.blade.php
@@ -19,7 +19,14 @@
          <td>{{ $w->no_kk }}</td>
          <td>{{ $w->jenik_kelamin }}</td>
          <td>{{ $w->alamat }}</td>
-         <td><a href="/warga/{{ $w->id }}/edit">Edit</a></td>
+         <td>
+               <a href="/warga/{{ $w->id }}/edit">Edit</a>
+               <form action="/warga/{{ $w->id }}" method="POST">
+                    @method('delete')
+                    @csrf
+                    <input type="submit" value="Delete">
+               </form>
+          </td>
     </tr>
     @endforeach
 </table>
